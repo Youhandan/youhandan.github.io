0aa26dbc4ee733d646657fac7a42793d774a014e
youhandan <youhandan@163.com>
2017-02-13 14:57:23 +0800

    Delete baidu analyst and add cnzz analyst

--- js/baidu_analyst.js
+++
@@ -1,7 +0,0 @@
- var _hmt = _hmt || [];
-    (function() {
-    var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?b6951e7cbf8d59a0725f41ec13a5207c";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
-})();
---
+++ js/cnzz_analyst.js
@@ -0,0 +1,4 @@
+var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(
+    unescape("%3Cspan id='cnzz_stat_icon_1261250562'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s95.cnzz.com/z_stat.php%3Fid%3D1261250562%26show%3Dpic' type='text/javascript'%3E%3C/script%3E")
+);

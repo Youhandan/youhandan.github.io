c50167a762652b3bfd47ad0f08170d482bbc7afc
youhandan <youhandan@163.com>
2017-02-13 15:35:33 +0800

    Modify cnzz analyst

--- js/cnzz_analyst.js
+++ js/cnzz_analyst.js
@@ -1,4 +1,7 @@
-var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-document.write(
-    unescape("%3Cspan id='cnzz_stat_icon_1261250562'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s95.cnzz.com/z_stat.php%3Fid%3D1261250562%26show%3Dpic' type='text/javascript'%3E%3C/script%3E")
-);
+var cnzz_s_tag = document.createElement('script');
+cnzz_s_tag.type = 'text/javascript';
+cnzz_s_tag.async = true;
+cnzz_s_tag.charset = "utf-8";
+cnzz_s_tag.src = "https://w.cnzz.com/c.php?id= XXXXXXXX &async=1";
+    var root_s = document.getElementsByTagName('script')[0];
+root_s.parentNode.insertBefore(cnzz_s_tag, root_s);

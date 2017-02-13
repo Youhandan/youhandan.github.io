c39212c01ad3de2f20e812a81a74f1ba341ef7bd
youhandan <youhandan@163.com>
2017-02-13 16:37:10 +0800

    Delete cnzz analyst

--- js/cnzz_analyst.js
+++
@@ -1,7 +0,0 @@
-var cnzz_s_tag = document.createElement('script');
-cnzz_s_tag.type = 'text/javascript';
-cnzz_s_tag.async = true;
-cnzz_s_tag.charset = "utf-8";
-cnzz_s_tag.src = "https://w.cnzz.com/c.php?id= XXXXXXXX &async=1";
-    var root_s = document.getElementsByTagName('script')[0];
-root_s.parentNode.insertBefore(cnzz_s_tag, root_s);

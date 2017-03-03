85dcbe94c80b75ca66ebaf41c2c7958a6394ca73
youhandan <youhandan@163.com>
2017-03-03 16:59:55 +0800

    recover comment code

--- js/comments.js
+++ js/comments.js
@@ -1,9 +1,9 @@
 var duoshuoQuery = {short_name:"youhandan"};
-(function() {
+$(document).ready(function() {
     var ds = document.createElement('script');
     ds.type = 'text/javascript';ds.async = true;
-    ds.src = 'http://static.duoshuo.com/embed.js';
+    ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
     ds.charset = 'UTF-8';
     (document.getElementsByTagName('head')[0]
     || document.getElementsByTagName('body')[0]).appendChild(ds);
-})();
+})
